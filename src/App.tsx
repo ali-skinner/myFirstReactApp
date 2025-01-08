ab03f601cc458420dca28c4395783a7ffa278a19
ali-skinner <ali.skinner.15@gmail.com>
2025-01-07 21:03:55 -0600

    learning

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,37 +6,54 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>``
+      <Timer />
+      <Users />
+    </>
   )
 }
 
 function Timer() {
-const [remainingTime, setRemainingTime] = useState(21)
+  const [remainingTime, setRemainingTime] = useState(21)
 
 
 }
 
+function Users() {
+  const theUsers = [
+    { name: "Judy", age: 45, hobbies: [" coding", " reading", " doing stuff"] },
+    { name: "Maude", age: 5, hobbies: ["nothing", " everything", " something"] },
+    { name: "Calvin", age: 20, hobbies: ["clothes", "journaling", "painting"] },
+    { name: "Friend", age: 38, hobbies: ["traveling", "walking", "sleeping"] },
+  ]
 
+  return (
+    <div>
+      {theUsers.map((user) => {
+        return (
+          <div style={{ width: "50vw", height: "15vw", border: "solid 20px pink" }}>
+            <h2>{user.name}</h2>
+            <h4>age: {user.age}</h4>
+            {user.hobbies.map((hobby) => {
+              return (
+                <span>{hobby}</span>
+              )
+            })}
+          </div>
+        )
+      }
+
+      )}
+    </div>
+  )
+}
+
+function Randomness() {
+  const randomStuff = 
+  
+  return (
+    {Math.floor(Math.random()* 500)}
+  )
+}
 
 
 export default App
