e0a883849c20a2f6fbb1032d054656362ee4541b
ali-skinner <ali.skinner.15@gmail.com>
2025-01-13 18:15:26 -0600

    mui

--- src/App.tsx
+++ src/App.tsx
@@ -2,58 +2,67 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(16)
-
-  return (
-    <>
-      <Timer />
-      <Users />
-    </>
-  )
-}
-
-function Timer() {
-  const [remainingTime, setRemainingTime] = useState(21)
+  // const [count, setCount] = useState(16)
 
 
-}
 
-function Users() {
-  const theUsers = [
-    { name: "Judy", age: 45, hobbies: [" coding", " reading", " doing stuff"] },
-    { name: "Maude", age: 5, hobbies: ["nothing", " everything", " something"] },
-    { name: "Calvin", age: 20, hobbies: ["clothes", "journaling", "painting"] },
-    { name: "Friend", age: 38, hobbies: ["traveling", "walking", "sleeping"] },
-  ]
-
-  return (
-    <div>
-      {theUsers.map((user) => {
-        return (
-          <div style={{ width: "50vw", height: "15vw", border: "solid 20px pink" }}>
-            <h2>{user.name}</h2>
-            <h4>age: {user.age}</h4>
-            {user.hobbies.map((hobby) => {
-              return (
-                <span>{hobby}</span>
-              )
-            })}
-          </div>
-        )
-      }
-
-      )}
-    </div>
-  )
-}
-
-function Randomness() {
-  const randomStuff = 
   
-  return (
-    {Math.floor(Math.random()* 500)}
-  )
 }
+
+
+
+
+
+//   return (
+//     <>
+//       <Timer />
+//       <Users />
+//     </>
+//   )
+// }
+
+// function Timer() {
+//   const [remainingTime, setRemainingTime] = useState(21)
+
+
+// }
+
+// function Users() {
+//   const theUsers = [
+//     { name: "Judy", age: 45, hobbies: [" coding", " reading", " doing stuff"] },
+//     { name: "Maude", age: 5, hobbies: ["nothing", " everything", " something"] },
+//     { name: "Calvin", age: 20, hobbies: ["clothes", "journaling", "painting"] },
+//     { name: "Friend", age: 38, hobbies: ["traveling", "walking", "sleeping"] },
+//   ]
+
+//   return (
+//     <div>
+//       {theUsers.map((user) => {
+//         return (
+//           <div style={{ width: "50vw", height: "15vw", border: "solid 20px pink" }}>
+//             <h2>{user.name}</h2>
+//             <h4>age: {user.age}</h4>
+//             {user.hobbies.map((hobby) => {
+//               return (
+//                 <span>{hobby}</span>
+//               )
+//             })}
+//           </div>
+//         )
+//       }
+
+//       )}
+//     </div>
+//   )
+// }
+
+// function Randomness() {
+//   const randomStuff = 
+  
+//   return (
+//     {Math.floor(Math.random()* 500)}
+//   )
+// }
 
 
 export default App
