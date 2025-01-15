0485b45ba1dc8c8eb7204c79ae0e6c0d4d2965eb
ali-skinner <ali.skinner.15@gmail.com>
2025-01-15 13:43:32 -0600

    sml prgrs

--- src/App.tsx
+++ src/App.tsx
@@ -16,44 +16,75 @@ function App() {
   // add props to pass data into components :)
   return (
     <>
-      <MyButton />
+      {/* <MyButton /> */}
       <TodoInputForm />
-      <OwnerSelect />
+      {/* <OwnerSelect />
       <ShowCompleted />
-      <ToDoList />
+      <ToDoList /> */}
     </>
   );
 };
 //END OF THE APP FUNCTION
+function TodoInputForm() {
+  return (
+    <div>
+      <label htmlFor='todo'>
+        Task:
+        <input id='todo' />
+      </label>
+
+      <label htmlFor='owner'>
+        Task Owner:
+        <input id='owner' />
+      </label>
+    </div>
+  )
+}
+
+
+//Add todo funct -- NEEDS WORK -- Tanner has this in the App funct on initial draft
+// const addTodo = () => {
+//   if (todos.title !== '') {
+//     setTodos([...todos,])
+//   } return todos
+// }
+
+//Remove todo funct -- NEEDS WORK-- Tanner has this in the App funct on initial draft
+// const removeTodo = () => {
+//   if (todos.title !== '') {
+//     setTodos([...todos,])
+//   } return todos
+// }
+
+//MAKE A BUTTON
+//completyes can be calc by todos.filter(t => t.completed).length
+//incompletes can be calc by todos.filter(t => !t.completed).length
+//takes a totdo list and maps and looks for a speciic totdo, makrs it completed
+
+// function MyButton() {
+//   const
+
+//   return (
+//     <>
+//       <button style={{
+//         backgroundColor: 'orange',
+//         color: 'white',
+//         border: 'black solid 3px',
+//         borderRadius: '5px',
+//       }}
+//         onClick={() => { addTodo }}
+//       >
+//         Add to the list
+//       </button>
+//     </>
+//   )
+// }
 
 //Random for ID generation
 const random = () => {
   return Math.floor(Math.random() * 1_000_000_000)
 }
 
-//Add todo funct -- NEEDS WORK
-const addTodo = () => {
-  if (todos.title !== '') {
-    setTodos([...todos,])
-  }
-}
-
-//MAKE A BUTTON
-//completyes can be calc by todos.filter(t => t.completed).length
-//incompletes can be calc by todos.filter(t => !t.completed).length
-//takes a totdo list and maps and looks for a speciic totdo, makrs it completed
-function MyButton() {
-  const
-
-  return (
-
-    <button onClick={() => do something }>
-      Completed
-    </button>
-
-
-  )
-}
 
 export default App
 
