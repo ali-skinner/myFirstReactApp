e799e6c63a5b2fd796a28d545ef3fd0487833e60
ali-skinner <ali.skinner.15@gmail.com>
2025-01-29 19:57:36 -0600

    outlines and files for api

--- api-giphy-project/api-giphy-project/src/App.jsx
+++ api-giphy-project/api-giphy-project/src/App.jsx
@@ -16,11 +16,17 @@ function App() {
 
 export default App;
 // set up routes for my 3 pages (login, search, favs)
-//login page needs username and password -- store these values in state
-//once logged in, set value to true
+
+//login page 
+// --> needs username and password -- store these values in state
+//---> once logged in, set value to true
+
+//search page
 // set up the search page with fetch similiar function
-// search  bar to search for gifs
+// search bar to search for gifs
 // fetch the gifs that match the query
+// need trending endpoint --> automatically populates the most relevant and engaging content each and every day
+// need search endpoint --> ets users search our library of millions of GIFs and Stickers by entering a word or phrase.
 // button to mark gifs as favorites
 // mark favs and store on the favs page
 // add api giphy key
@@ -28,7 +34,17 @@ export default App;
 //https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${search}&limit=9
 
 
-//bonus: pre-existing favorites for different users that are hard coded into the application. Once you do that, add the following functionality:
+
+//HOMEWORK:
+// The application should have the following
+
+// A login page to log in to the application.
+// Once logged in, store the user's username into state.
+// A search page that allows you to type in a search field and bring up gifs that match that query.
+// The ability to add favorites based off search results.
+// A favorites page that allows you to see your favorites.
+
+// pre-existing favorites for different users that are hard coded into the application. Once you do that, add the following functionality:
 
 // Allow someone to go to: /user/username and based off the username, pull that user's favorites from the pre-built list.
 // Strip out any code to different files as needed and think about how you might protect against any errors that might pop up.
--- api-giphy-project/api-giphy-project/src/Components/GiphySearchPage.jsx
+++ api-giphy-project/api-giphy-project/src/Components/GiphySearchPage.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
+const API_KEY = "3ZkAsAtjGs6SJgjRttscxEyJ8pq6odBV"
+//https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${search}&limit=9
+
+
 function GiphySearchPage(){
      
     return(
