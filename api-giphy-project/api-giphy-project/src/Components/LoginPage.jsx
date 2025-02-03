fb33cdf26a85647350640c0a6a8355b5165c3222
ali-skinner <ali.skinner.15@gmail.com>
2025-02-03 14:44:02 -0600

    login v1

--- api-giphy-project/api-giphy-project/src/Components/LoginPage.jsx
+++ api-giphy-project/api-giphy-project/src/Components/LoginPage.jsx
@@ -1,20 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
-function LoginPage () {
+function LoginPage() {
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [username, setUsername] = useState();
 
-    return(
+    return (
         <>
-        <div className="login-Container">
-            <h2 className="login-Header">Sign In</h2>
-            <div className="login-Input"> Username </div>
-            <div className="login-Input"> Password </div>
-            <div className="login-Input"> Register </div>
-        </div>
+            <form>
+                <div className="login-Container">
+                    <h2 className="login-Header">Sign In</h2>
+                    <div className="login-Input">
+                        <label htmlFor="username">Username</label>
+                        <input value={username} type="text" placeholder="username" name="username" id="username" />
+                    </div>
+                    <div className="login-Input"> Password </div>
+                        <input value={password} type="text" placeholder="password" name="password" id="password" />
+                    <div className="login-Input"> Register </div>
+                </div>
+            </form>
+            <div className="login-Button-Container">
+                <button className="button-login">Log In</button>
+                <button className="button-login">Create Username</button>
+            </div>
 
-        <div className="login-Button-Container">
-            <button className="button-login">Login In</button>
-            <button className="button-login">Create Username</button>
-        </div>
         </>
 
     );
