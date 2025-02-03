45a10d476e05eb391561a935c7704b46ca36ac13
ali-skinner <ali.skinner.15@gmail.com>
2025-02-03 16:23:16 -0600

    login v1

--- api-giphy-project/api-giphy-project/src/Components/LoginPage.jsx
+++ api-giphy-project/api-giphy-project/src/Components/LoginPage.jsx
@@ -4,6 +4,7 @@ function LoginPage() {
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
     const [username, setUsername] = useState();
+    const [register, setRegister] = useState();
 
     return (
         <>
@@ -12,20 +13,21 @@ function LoginPage() {
                     <h2 className="login-Header">Sign In</h2>
                     <div className="login-Input">
                         <label htmlFor="username">Username</label>
-                        <input value={username} type="text" placeholder="username" name="username" id="username" />
+                        <input value={username} onChange={(e) => setUsername(e.target.value)} type="text" placeholder="username" name="username" id="username" />
                     </div>
-                    <div className="login-Input"> Password </div>
-                        <input value={password} type="text" placeholder="password" name="password" id="password" />
-                    <div className="login-Input"> Register </div>
+                    <div className="login-Input">
+                        <label htmlFor="password">Password</label>
+                        <input value={password} onChange={(e) => setPassword(e.target.value)} type="text" placeholder="password" name="password" id="password" />
+                        <button className="button-login">Log In</button>
+                    </div>
+                    <div className="login-Input">
+                        <label htmlFor="register">Register</label>
+                        <input value={register} onChange={(e) => setRegister(e.target.value)} type="text" placeholder="register" name="register" id="register" />
+                        <button className="button-login">Create Username</button>
+                    </div>
                 </div>
             </form>
-            <div className="login-Button-Container">
-                <button className="button-login">Log In</button>
-                <button className="button-login">Create Username</button>
-            </div>
-
         </>
-
     );
 };
 
